58e579b786595e5b2371b7a0a590eb47a12e89ea
Umar Khan <umarkhandev@gmail.com>
2019-02-23 17:21:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,9 @@ require 'pry'
 def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
-  else number % 5 == 0
+  elsif number % 5 == 0
       "Buzz"
+  else number % 3 && 5 == 0
+    "FizzBuzz"
   end
 end
